2a4eb0a7c91822184ab2e2373b3d6ce166ddba46
Kristopher Mazzini <kristophermazzini.sc@gmail.com>
2022-10-15 23:13:19 -0300

    add first settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,11 @@
 import { ThemeProvider } from 'styled-components'
 
-import { Button } from './Components/Button'
-
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant="primary" />
-      <Button variant="secondary" />
-      <Button variant="danger" />
-      <Button variant="success" />
-      <Button />
-
       <GlobalStyle />
     </ThemeProvider>
   )
